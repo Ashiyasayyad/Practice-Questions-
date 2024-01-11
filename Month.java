a675535f28d8ea16a8a8b820460f69e3b5ffe5ad
Ashiyasayyad <141029360+Ashiyasayyad@users.noreply.github.com>
2024-01-11 19:07:17 +0530

    Create Month.java

---
+++ Month.java
@@ -0,0 +1,68 @@
+// Display month name
+		System.out.println("Enter the month:");
+		switch(month)
+		{
+		Case 1 :
+		{
+			System.out.println("January");
+			break;
+		}
+		Case 2:
+		{
+			System.out.println("February");
+			break;
+		}
+		Case 3:
+		{
+			System.out.println("March");
+			break;
+		}
+		Case 4:
+		{
+			System.out.println("April");
+			break;
+		}
+		Case 5:
+		{
+			System.out.println("May");
+			break;
+		}
+		Case 6:
+		{
+			System.out.println("June");
+			break;
+		}
+		Case 7:
+		{
+			System.out.println("July");
+			break;
+		}
+		Case 8:
+		{
+			System.out.println("August");
+			break;
+		}
+		Case 9:
+		{
+			System.out.println("september");
+			break;
+		}
+		Case 10:
+		{
+			System.out.println("october");
+			break;
+		}
+		Case 11:
+		{
+			System.out.println("november");
+			break;
+		}
+		Case 12:
+		{
+			System.out.println("December");
+			break;
+		}
+		default:
+			System.out.println("Invalid month:");
+			break;
+    }
